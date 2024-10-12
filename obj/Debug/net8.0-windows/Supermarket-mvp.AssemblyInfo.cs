6b583e3b6f2d761a91015551c500710052f1a3e3
Hans Lemmel C <hanslemmel92@gmail.com>
2024-10-12 15:30:21 -0500

    Categories Repository

--- obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e87561ac3462b7225eef657343088f68c111997e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41fb5f46d61974ee2b8262ce8bb30270f491fafb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
